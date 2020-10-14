b073a1ed882dbd1322d17469487c8ad7e0279ef3
lipskydan <lipsky@knu.ua>
2020-10-14 19:16:06 +0300

    the weights of the logistic cost function, classification costs

--- pythonProject/sklearnTrain.py
+++ pythonProject/sklearnTrain.py
@@ -82,3 +82,32 @@ ax.yaxis.grid(True)
 plt.tight_layout()
 plt.savefig('images/sklearnTrain/sigmoid_function.png', dpi=300)
 plt.show()
+
+
+# the weights of the logistic cost function, classification costs
+
+def cost_1(z):
+    return - np.log(sigmoid(z))
+
+
+def cost_0(z):
+    return - np.log(1 - sigmoid(z))
+
+
+z = np.arange(-10, 10, 0.1)
+phi_z = sigmoid(z)
+
+c1 = [cost_1(x) for x in z]
+plt.plot(phi_z, c1, label='J(w) if y=1')
+
+c0 = [cost_0(x) for x in z]
+plt.plot(phi_z, c0, linestyle='--', label='J(w) if y=0')
+
+plt.ylim(0.0, 5.1)
+plt.xlim([0, 1])
+plt.xlabel('$\phi$(z)')
+plt.ylabel('J(w)')
+plt.legend(loc='upper center')
+plt.tight_layout()
+plt.savefig('images/sklearnTrain/costs.png', dpi=300)
+plt.show()
