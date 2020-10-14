2ef63b79acfe8f53cd523dfc710c17028c903e88
lipskydan <lipsky@knu.ua>
2020-10-14 19:24:18 +0300

    add def fit

--- pythonProject/logisticRegressionGD.py
+++ pythonProject/logisticRegressionGD.py
@@ -24,3 +24,36 @@ class LogisticRegressionGD(object):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+
+        Parameters
+        ----------
+        X : {array-like}, shape = [n_samples, n_features]
+          Training vectors, where n_samples is the number of samples and
+          n_features is the number of features.
+        y : array-like, shape = [n_samples]
+          Target values.
+
+        Returns
+        -------
+        self : object
+
+        """
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+
+            # note that we compute the logistic `cost` now
+            # instead of the sum of squared errors cost
+            cost = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output)))
+            self.cost_.append(cost)
+        return self
