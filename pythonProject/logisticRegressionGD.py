26d2e2d1bcface2c089e49cfd447b8e0ef1951c7
lipskydan <lipsky@knu.ua>
2020-10-14 19:21:01 +0300

    add logisticRegressionGD.py

---
+++ pythonProject/logisticRegressionGD.py
@@ -0,0 +1,26 @@
+class LogisticRegressionGD(object):
+    """Logistic Regression Classifier using gradient descent.
+
+    Parameters
+    ------------
+    eta : float
+      Learning rate (between 0.0 and 1.0)
+    n_iter : int
+      Passes over the training dataset.
+    random_state : int
+      Random number generator seed for random weight
+      initialization.
+
+
+    Attributes
+    -----------
+    w_ : 1d-array
+      Weights after fitting.
+    cost_ : list
+      Sum-of-squares cost function value in each epoch.
+
+    """
+    def __init__(self, eta=0.05, n_iter=100, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
