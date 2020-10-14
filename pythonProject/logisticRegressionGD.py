f9818ff6c04f4dcd883361f8af5bafea84360c2c
lipskydan <lipsky@knu.ua>
2020-10-14 19:25:36 +0300

    add def predict

--- pythonProject/logisticRegressionGD.py
+++ pythonProject/logisticRegressionGD.py
@@ -67,3 +67,9 @@ class LogisticRegressionGD(object):
     def activation(self, z):
         """Compute logistic sigmoid activation"""
         return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+        # equivalent to:
+        # return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
