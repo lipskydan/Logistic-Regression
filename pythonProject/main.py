689f2f3d17e7d5ce8bcef1b6c153fd975a64db54
lipskydan <lipsky@knu.ua>
2020-10-14 11:44:54 +0300

    Training a perceptron via scikit-learn base perceptron

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -3,6 +3,11 @@ import numpy as np
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+from solutionAreaGraph import plot_decision_regions
 
 # First steps with scikit-learn
 # Loading the Iris dataset from scikit-learn.
@@ -11,7 +16,7 @@ iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
 y = iris.target
 
-print('Class labels:', np.unique(y))
+print('Метки классов:', np.unique(y))
 
 
 # Splitting data into 70% training and 30% test data:
@@ -19,9 +24,9 @@ print('Class labels:', np.unique(y))
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=1, stratify=y)
 
-print('Labels counts in y:', np.bincount(y))
-print('Labels counts in y_train:', np.bincount(y_train))
-print('Labels counts in y_test:', np.bincount(y_test))
+print('Количество меток в y:', np.bincount(y))
+print('Количество меток в y_train:', np.bincount(y_train))
+print('Количество меток в y_test:', np.bincount(y_test))
 
 # Standardizing the features:
 
@@ -32,8 +37,29 @@ X_test_std = sc.transform(X_test)
 
 # Training a perceptron via scikit-learn base perceptron
 
-ppn = Perceptron(n_iter=40, eta0=0.1, random_state=1)
+ppn = Perceptron(max_iter=40, eta0=0.1, random_state=1)
 ppn.fit(X_train_std, y_train)
+
+y_pred = ppn.predict(X_test_std)
+print(f'Неправильно классифицированных образцов: {(y_test != y_pred).sum()} % ')
+
+print(f'Правильность: {accuracy_score(y_test, y_pred)} %')
+print(f'Правильность: {ppn.score(X_test_std, y_test)} %')
+
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X=X_combined_std, y=y_combined,
+                      classifier=ppn, test_idx=range(105, 150))
+plt.xlabel('длина лепестка [стандартизированная]')
+plt.ylabel('ширина лепестка [стандартизированная]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+#plt.savefig('images/03_01.png', dpi=300)
+plt.show()
+
 
 # class LogisticRegressionGD(object):
 #     """Logistic Regression Classifier using gradient descent.
