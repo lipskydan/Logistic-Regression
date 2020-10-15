cdbf71d6b3f7e76e60b9bfcecdfb6e8c786049a4
lipskydan <lipsky@knu.ua>
2020-10-15 10:10:45 +0300

    add area_solution_regression_model

--- pythonProject/logisticRegressionGD.py
+++ pythonProject/logisticRegressionGD.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+
+from solutionAreaGraph import plot_decision_regions
+from sklearnTrain import X_train, y_train
+
+
 class LogisticRegressionGD(object):
     """Logistic Regression Classifier using gradient descent.
 
@@ -70,6 +81,26 @@ class LogisticRegressionGD(object):
 
     def predict(self, X):
         """Return class label after unit step"""
-        return np.where(self.net_input(X) >= 0.0, 1, 0)
+        # return np.where(self.net_input(X) >= 0.0, 1, 0)
         # equivalent to:
-        # return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
+
+X_train_01_subset = X_train[(y_train == 0) | (y_train == 1)]
+y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+
+lrgd = LogisticRegressionGD(eta=0.05, n_iter=1000, random_state=1)
+lrgd.fit(X_train_01_subset,
+         y_train_01_subset)
+
+plot_decision_regions(X=X_train_01_subset,
+                      y=y_train_01_subset,
+                      classifier=lrgd)
+
+plt.xlabel('длина лепестка [стандартизированнаяч]')
+plt.ylabel('ширина лепестка [стандартизированнаяч]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+plt.savefig('images/area_solution_regression_model.png', dpi=300)
+plt.show()
